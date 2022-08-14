2d9700542f76cbe739c1cdfe172bf5db23ad83a8
Mohammed-abdellah El hamra <mel-hamr@e2r9p3.1337.ma>
2022-08-14 18:09:34 +0100

    lesson 24

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,16 +1,16 @@
 import { useEffect, useState} from 'react'
 
 const useFetch = (url) =>{
-
 	const [data, setData] = useState(null);
-
+	
 	const [isLoading , setIsLoading] = useState(true);
-
+	
 	const [error,setError] = useState(null)
-
+	
     useEffect(() => {
+		const abortCont = new AbortController();
 		setTimeout(() =>{
-			fetch(url)
+			fetch(url , {signal : abortCont.signal})
 			.then(res => {
 				if(!res.ok)
 					throw Error('could not fetch the data from the server')
@@ -22,11 +22,20 @@ const useFetch = (url) =>{
 				setError(null)
 			})
 			.catch( err => {
-				setIsLoading(false)
-				setError(err.message)
+				if (err.message === 'AbortError'){
+					console.log('fetch aborted')
+				}
+				else{
+					setIsLoading(false)
+					setError(err.message)
+				}
+
 			})
 		},1000)
-	},[]);
+		return () => {
+			abortCont.abort()
+		}
+	},[url]);
     return {data , isLoading , error}
 }
 export default useFetch
